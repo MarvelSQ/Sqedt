ae81a975992f7d0650d118bd9ec07591389ba8f3
MarvelSQ <sun1993hlj@hotmail.com>
2017-07-11 00:08:01 +0800

    fix some function name spell add README.me

---
+++ README.md
@@ -0,0 +1,17 @@
+#SQEDT:a Html VisualEditor
+
+this is first Html Component
+
+### Function
+
+* add/remove **BOLD**/*ITALIC*/~~STRIKE~~
+* change font size by Num
+* change font size to bigger/smaller
+* change font color
+
+more function will be added
+
+
+### Learing
+
+i'm still learning JS and other front-end stuff
--- sqedt/sqedt.js
+++ sqedt/sqedt.js
@@ -35,18 +35,18 @@ let SQEdt = function (options) {
         throw new Error('SQEdt must have a Element')
     }
 
-    this.el.contentEditable = true
-    this.el.innerHTML = '<div class="line"><span style="font-size: 16px">asdasdasdasdasd</span><span>ertertertertert</span></div><div class="line"><span><b>asdasdasdasd</b></span></div><div class="line"><span>sadsasdasdas</span></div>'
-    this.el.classList.add('sqedt')
-    this.sl = window.getSelection()
+    this.el.contentEditable = true;
+    this.el.innerHTML = '<div class="line"><span style="font-size: 16px">asdasdasdasdasd</span><span>ertertertertert</span></div><div class="line"><span><b>asdasdasdasd</b></span></div><div class="line"><span>sadsasdasdas</span></div>';
+    this.el.classList.add('sqedt');
+    this.sl = window.getSelection();
 
     this.el.addEventListener('selectstart', function (e) {
         //console.log(e)
-    })
+    });
 
     this.el.addEventListener('blur', function (e) {
         //console.log(e)
-    })
+    });
 
     this.el.addEventListener('input', function (e) {
         if (e.target.innerText.length === 0) {
@@ -58,25 +58,25 @@ let SQEdt = function (options) {
             }
         }
     })
-}
+};
 
 let EdtItem = function (range) {
-    this.startContainer = range.startContainer
-    this.startOffset = range.startOffset
-    this.endContainer = range.endContainer
-    this.endOffset = range.endOffset
-    this.commonContainer = range.commonAncestorContainer
-    this.startParent = range.startContainer.parentNode
-    this.endParent = range.endContainer.parentNode
-}
+    this.startContainer = range.startContainer;
+    this.startOffset = range.startOffset;
+    this.endContainer = range.endContainer;
+    this.endOffset = range.endOffset;
+    this.commonContainer = range.commonAncestorContainer;
+    this.startParent = range.startContainer.parentNode;
+    this.endParent = range.endContainer.parentNode;
+};
 
 SQEdt.prototype = {
     check: function () {
-        let target = this.sl.focusNode.parentNode
+        let target = this.sl.focusNode.parentNode;
         if (target === this.el) {
             return true
         }
-        let els = this.el.querySelectorAll(this.sl.focusNode.parentNode.tagName)
+        let els = this.el.querySelectorAll(this.sl.focusNode.parentNode.tagName);
         for (let i = 0; i < els.length; i++) {
             if (target === els.item(i)) {
                 return true
@@ -85,15 +85,15 @@ SQEdt.prototype = {
         return false
     },
     threeCut: function (item, range, include) {
-        let c = range.cloneContents()
-        range.deleteContents()
+        let c = range.cloneContents();
+        range.deleteContents();
         if (include) {
             range.setStartBefore(item.commonContainer)
         } else {
             range.setStart(item.commonContainer, 0)
         }
-        let f = range.cloneContents()
-        range.deleteContents()
+        let f = range.cloneContents();
+        range.deleteContents();
         if (include) {
             range.setEndAfter(item.commonContainer)
         } else {
@@ -106,9 +106,9 @@ SQEdt.prototype = {
         }
 
 
-        let e = range.cloneContents()
-        range.deleteContents()
-        console.log([f, c, e])
+        let e = range.cloneContents();
+        range.deleteContents();
+        console.log([f, c, e]);
         return {
             before: f,
             content: c,
@@ -116,7 +116,7 @@ SQEdt.prototype = {
         }
     },
     getLine: function (el) {
-        let parent = el
+        let parent = el;
         for (let i = 0; i < 5; i++) {
             if (parent.nodeName !== 'DIV') {
                 parent = parent.parentNode
@@ -125,19 +125,19 @@ SQEdt.prototype = {
         return parent
     },
     getSpan: function (parent, el) {
-        let wrap = el
+        let wrap = el;
         let tmp;
         for (let i = 0; i < 4; i++) {
             if (parent.nodeName !== 'SPAN') {
-                let outer = parent.parentNode.cloneNode(false)
-                parent = parent.parentNode
-                tmp = wrap
-                wrap = outer
+                let outer = parent.parentNode.cloneNode(false);
+                parent = parent.parentNode;
+                tmp = wrap;
+                wrap = outer;
                 wrap.appendChild(tmp)
             } else {
                 if (wrap.nodeName !== 'SPAN') {
-                    tmp = wrap
-                    wrap = parent.cloneNode(false)
+                    tmp = wrap;
+                    wrap = parent.cloneNode(false);
                     wrap.appendChild(tmp)
                 }
             }
@@ -145,7 +145,7 @@ SQEdt.prototype = {
         return wrap
     },
     getSpanParent: function (el) {
-        let parent = el
+        let parent = el;
         for (let i = 0; i < 4; i++) {
             if (parent.nodeName !== 'SPAN') {
                 parent = parent.parentNode
@@ -154,7 +154,7 @@ SQEdt.prototype = {
         return parent
     },
     checkNodeIsAllBelong: function (node, nodename) {
-        let nodes = node.querySelectorAll(nodename)
+        let nodes = node.querySelectorAll(nodename);
         for (let i = 0; i < nodes.length; i++) {
             nodes[i].remove()
         }
@@ -171,20 +171,20 @@ SQEdt.prototype = {
         nodesParent.normalize()
     },
     addTag: function (node, nodename) {
-        return nodename === 'b' ? this.addBoldTag(node) : nodename === 'i' ? this.addItalyTag(node) : nodename == 's' ? this.addSlashTag(node) : ''
+        return nodename === 'b' ? this.addBoldTag(node) : nodename === 'i' ? this.addItalicTag(node) : nodename === 's' ? this.addStrikeTag(node) : ''
     },
     addBoldTag: function (node) {//node must be span
         node.innerHTML = '<b>' + node.innerHTML.replace('<b>', '').replace('</b>', '') + '</b>'
     },
-    addItalyTag: function (node) {//node must be span
+    addItalicTag: function (node) {//node must be span
         for (let i = 0; i < node.childNodes.length; i++) {
-            let tmp = node.childNodes[i]
+            let tmp = node.childNodes[i];
             if (tmp.innerText === '') {
                 node.replaceChild(document.createTextNode(''), tmp)
             } else {
                 if (tmp.nodeName === '#text' || tmp.nodeName === 'S') {
-                    let i = document.createElement('i')
-                    i.appendChild(tmp.cloneNode(true))
+                    let i = document.createElement('i');
+                    i.appendChild(tmp.cloneNode(true));
                     node.replaceChild(i, tmp)
                 }
                 if (tmp.nodeName === 'B') {
@@ -194,15 +194,15 @@ SQEdt.prototype = {
         }
         node.normalize()
     },
-    addSlashTag: function (node) {//node must be span
+    addStrikeTag: function (node) {//node must be span
         for (let i = 0; i < node.childNodes.length; i++) {
-            let tmp = node.childNodes[i]
+            let tmp = node.childNodes[i];
             if (tmp.innerText === '') {
                 node.replaceChild(document.createTextNode(''), tmp)
             } else {
                 if (tmp.nodeName === '#text') {
-                    let s = document.createElement('s')
-                    s.appendChild(tmp.cloneNode(true))
+                    let s = document.createElement('s');
+                    s.appendChild(tmp.cloneNode(true));
                     node.replaceChild(s, tmp)
                 }
                 if (tmp.nodeName === 'B' || tmp.nodeName === 'I') {
@@ -213,8 +213,8 @@ SQEdt.prototype = {
         node.normalize()
     },
     removeTag: function (node, nodename) {
-        let s = eval('/<' + nodename + '>/g')
-        let e = eval('/<\\/' + nodename + '>/g')
+        let s = eval('/<' + nodename + '>/g');
+        let e = eval('/<\\/' + nodename + '>/g');
         node.innerHTML = node.innerHTML.replace(s, '').replace(e, '')
     },
     removeTagInFragment: function (nodeList, nodename) {
@@ -230,7 +230,7 @@ SQEdt.prototype = {
                 nodeParent.childNodes[i] = document.createTextNode('')
             } else if (nodeParent.childNodes[i].nodeName !== '#text') {
                 if (i >= 1 && nodeParent.childNodes[i].nodeName === nodeParent.childNodes[i - 1].nodeName && nodeParent.childNodes[i].style.cssText === nodeParent.childNodes[i - 1].style.cssText) {
-                    let len = nodeParent.childNodes[i].childNodes.length
+                    let len = nodeParent.childNodes[i].childNodes.length;
                     for (let index = 0; index < len; index++) {
                         nodeParent.childNodes[i - 1].appendChild(nodeParent.childNodes[i].childNodes[0])
                     }
@@ -255,11 +255,11 @@ SQEdt.prototype = {
     changeFontSize: function (node, font) {
         switch (font) {
             case 'up':
-                this.changeFontSizeByCount(node, true)
-                break
+                this.changeFontSizeByCount(node, true);
+                break;
             case 'down':
-                this.changeFontSizeByCount(node, false)
-                break
+                this.changeFontSizeByCount(node, false);
+                break;
             default:
                 if (typeof font === 'number') {
                     this.changeFontSizeByNum(node, font)
@@ -294,33 +294,33 @@ SQEdt.prototype = {
     },
 
     changeTag: function (options) {
-        console.log(new Date().getMilliseconds())
+        console.log(new Date().getMilliseconds());
         if (!this.check()) {
             return
         }
-        let range = this.sl.getRangeAt(0)
-        let item = new EdtItem(range)
+        let range = this.sl.getRangeAt(0);
+        let item = new EdtItem(range);
         //alert(this.sl.toString())
 
-        let isAll = false
+        let isAll = false;
         // let tmp = range.cloneContents()
         // range.deleteContents()
         // range.insertNode(tmp)
         switch (item.commonContainer.nodeName) {
             case 'DIV':
                 if (item.commonContainer.classList.contains('sqedt')) {
-                    let tmp = range.cloneContents()
+                    let tmp = range.cloneContents();
                     // range.deleteContents()
                     // range.insertNode(tmp)
 
-                    this.mergeTag(tmp)
+                    this.mergeTag(tmp);
 
                     if (options.tag) {
                         for (let i = 0; i < tmp.childNodes.length; i++) {
                             if (this.checkNodeIsAllBelong(tmp.childNodes[i].cloneNode(true), options.tag)) {
                                 isAll = true
                             } else {
-                                isAll = false
+                                isAll = false;
                                 break;
                             }
                         }
@@ -333,13 +333,13 @@ SQEdt.prototype = {
                         }
                     }
                     if (options.font) {
-                        console.log(options.font)
+                        console.log(options.font);
                         for (let i = 0; i < tmp.childNodes.length; i++) {
                             this.changeFontSizeInFragment(tmp.childNodes[i], options.font)
                         }
                     }
                     if (options.color) {
-                        console.log(options.color)
+                        console.log(options.color);
                         for (let i = 0; i < tmp.childNodes.length; i++) {
                             this.changeFontColorInFragment(tmp.childNodes[i], options.color)
                         }
@@ -347,21 +347,21 @@ SQEdt.prototype = {
 
 
 
-                    let first = tmp.childNodes[0]
-                    let last = tmp.childNodes[tmp.childNodes.length - 1]
-                    let start = first.children[0]
-                    let end = last.children[last.children.length - 1]
-                    tmp.childNodes[tmp.childNodes.length - 1].remove()
+                    let first = tmp.childNodes[0];
+                    let last = tmp.childNodes[tmp.childNodes.length - 1];
+                    let start = first.children[0];
+                    let end = last.children[last.children.length - 1];
+                    tmp.childNodes[tmp.childNodes.length - 1].remove();
                     if (tmp.childNodes[0]) {
                         tmp.childNodes[0].remove()
                     }
-                    range.deleteContents()
-                    let elnum = first.children.length
+                    range.deleteContents();
+                    let elnum = first.children.length;
                     for (let i = 0; i < elnum; i++) {
                         this.getLine(item.startContainer).appendChild(first.children[0])
                     }
-                    range.insertNode(tmp)
-                    elnum = last.children.length
+                    range.insertNode(tmp);
+                    elnum = last.children.length;
                     for (let i = 0; i < elnum; i++) {
                         this.getLine(item.endContainer).insertBefore(last.children[last.children.length - 1], this.getLine(item.endContainer).firstChild)//.appendChild()
                     }
@@ -377,7 +377,7 @@ SQEdt.prototype = {
                     }
 
                 } else if (item.commonContainer.classList.contains('line')) {
-                    let cuts = this.threeCut(item, range, false)
+                    let cuts = this.threeCut(item, range, false);
                     for (let i = cuts.before.childNodes.length - 1; i >= 0; i--) {
                         if (cuts.before.childNodes[i].innerText === '') {
                             cuts.before.childNodes[i].remove()
@@ -393,7 +393,7 @@ SQEdt.prototype = {
                         if (this.checkNodeIsAllBelong(cuts.content.childNodes[i].cloneNode(true), options.tag)) {
                             isAll = true
                         } else {
-                            isAll = false
+                            isAll = false;
                             break;
                         }
                     }
@@ -405,23 +405,23 @@ SQEdt.prototype = {
                         }
                     }
                     if (options.font) {
-                        console.log(options.font)
+                        console.log(options.font);
                         this.changeFontSizeInFragment(cuts.content, options.font)
                     }
                     if (options.color) {
-                        console.log(options.color)
+                        console.log(options.color);
                         this.changeFontColorInFragment(cuts.content, options.color)
                     }
 
-                    item.commonContainer.appendChild(cuts.before)
+                    item.commonContainer.appendChild(cuts.before);
 
-                    this.mergeTag(cuts.content)
+                    this.mergeTag(cuts.content);
 
-                    let slStart = cuts.content.firstChild
-                    let slEnd = cuts.content.lastChild
-                    item.commonContainer.appendChild(cuts.content)
-                    item.commonContainer.appendChild(cuts.after)
-                    range.setStartBefore(slStart)
+                    let slStart = cuts.content.firstChild;
+                    let slEnd = cuts.content.lastChild;
+                    item.commonContainer.appendChild(cuts.content);
+                    item.commonContainer.appendChild(cuts.after);
+                    range.setStartBefore(slStart);
                     range.setEndAfter(slEnd)
                 }
                 break;
@@ -430,10 +430,10 @@ SQEdt.prototype = {
             case 'B':
             case 'I':
             case 'S':
-                item.commonContainer.normalize()
-                let content = this.getSpan(item.commonContainer, range.cloneContents().childNodes[0])
-                item.commonContainer = this.getSpanParent(item.commonContainer)
-                let cuts = this.threeCut(item, range, true)
+                item.commonContainer.normalize();
+                let content = this.getSpan(item.commonContainer, range.cloneContents().childNodes[0]);
+                item.commonContainer = this.getSpanParent(item.commonContainer);
+                let cuts = this.threeCut(item, range, true);
 
                 if (options.tag) {
                     if (this.checkNodeIsAllBelong(content.cloneNode(true), options.tag)) {
@@ -443,11 +443,11 @@ SQEdt.prototype = {
                     }
                 }
                 if (options.font) {
-                    console.log(options.font)
+                    console.log(options.font);
                     this.changeFontSize(content, options.font)
                 }
                 if (options.color) {
-                    console.log(options.color)
+                    console.log(options.color);
                     this.changeFontColor(content, options.color)
                 }
 
@@ -463,19 +463,19 @@ SQEdt.prototype = {
                     }
                 }
 
-                this.mergeTag(content)
+                this.mergeTag(content);
 
 
-                range.insertNode(cuts.after)
-                range.insertNode(content)
-                range.insertNode(cuts.before)
+                range.insertNode(cuts.after);
+                range.insertNode(content);
+                range.insertNode(cuts.before);
                 //range.selectNode(content) //bug:first shift is successful,but second is wrong
-                range.setStartBefore(content.firstChild)
-                range.setEndAfter(content.lastChild)
+                range.setStartBefore(content.firstChild);
+                range.setEndAfter(content.lastChild);
                 break;
 
         }
-        this.el.focus()
+        this.el.focus();
         console.log(new Date().getMilliseconds())
     },
     bold: function () {
@@ -496,5 +496,5 @@ SQEdt.prototype = {
     color:function (color) {
         this.changeTag({color:color})
     }
-}
+};
 
