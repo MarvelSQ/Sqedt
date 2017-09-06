b220ab919edd062a58358373dd1257761a36c282
MBP <sun1993hlj@hotmail.com>
2017-09-06 14:40:49 +0800

    add babel and change readme

--- README.md
+++ README.md
@@ -16,57 +16,7 @@ more function will be added
 ### Get Start
 
 ***
-
-start
-```html
-<div id="editor"></div>
-<script src="sqedt/sqedt.js"></script>
-<script>
-    let editor = document.getElementById('editor');
-    let myEdt = new SQEdt({el:editor});
-</script>
-```
-
-**bold/italic/strike**
-```html
-<button onclick="myEdt.bold()"><b>Bold</b></button>
-<button onclick="myEdt.italic()"><i>Italy</i></button>
-<button onclick="myEdt.strike()"><s>Slash</s></button>
-```
-
-**bigger/smaller font size**
-```html
-<button onclick="myEdt.bigger()">A+</button>
-<button onclick="myEdt.smaller()">A-</button>
-```
-
-**change font color/size**
-```html
-<select id="font">
-    <option>12</option>
-    <option>14</option>
-    <option selected>16</option>
-    <option>18</option>
-    <option>20</option>
-</select>
-<select id="color">
-    <option value="#ff0000">red</option>
-    <option value="#0000ff">blue</option>
-    <option value="#00ff00">green</option>
-    <option value="#000000">black</option>
-    <option value="#888888">grey</option>
-</select>
-<script>
-    let fontChanger = document.getElementById('font');
-    let colorChanger = document.getElementById('color');
-    fontChanger.addEventListener('change',function (e) {
-        myEdt.changeTag({font:Number(this.value)})
-    });
-    colorChanger.addEventListener('change',function (e) {
-        myEdt.changeTag({color:this.value});
-    });
-</script>
-```
+to be continue
 
 
 ### Learning
---
+++ src/utils.js
@@ -0,0 +1,232 @@
+/**
+ * [isAllElements description]
+ * @param  {[type]}  type [description]
+ * @param  {[type]}  el   [description]
+ * @return {Boolean}      [description]
+ */
+const isAllElements = (type, el) => {
+  if (el.innerText !== '') {
+    let elc = el.cloneNode(true)
+    let array = elc.querySelectorAll(type)
+    for (let cell of array) {
+      cell.remove();
+    }
+    return elc.innerText === ''
+  } else {
+    return false;
+  }
+}
+
+/**
+ * [matchById description]
+ * @param  {[type]} el [description]
+ * @param  {[type]} id [description]
+ * @return {[type]}    [description]
+ */
+const matchById = (el, id) => {
+  if (el.nodeName === '#text') {
+    return false;
+  }
+  return el.id === id;
+}
+
+/**
+ * [matchByClass description]
+ * @param  {[type]} el        [description]
+ * @param  {[type]} className [description]
+ * @return {[type]}           [description]
+ */
+const matchByClass = (el, className) => {
+  if (el.nodeName === '#text') {
+    return false;
+  }
+  return el.classList.contains(className);
+}
+
+/**
+ * [matchByTag description]
+ * @param  {[type]} el  [description]
+ * @param  {[type]} tag [description]
+ * @return {[type]}     [description]
+ */
+const matchByTag = (el, tag) => {
+  if (el.nodeName === '#text') {
+    return false;
+  }
+  return el.nodeName.toLowerCase() === tag;
+}
+
+/**
+ * [judgeStart description]
+ * @param  {[type]} reg [description]
+ * @return {[type]}     [description]
+ */
+const judgeStart = reg => {
+  let start = reg.charAt(0);
+  let match = undefined;
+  switch (start) {
+    case '#':
+      return [matchById, reg.substr(1)];
+      break;
+    case '.':
+      return [matchByClass, reg.substr(1)];
+      break;
+    default:
+      return [matchByTag, reg];
+      break;
+  }
+}
+
+/**
+ * [removeEmpty description]
+ * @param  {HtmlCollection,NodeList,Array} collection this val must be live or inside some root constructure
+ * @return {[type]}            [description]
+ */
+const removeEmpty = function(collection) {
+  //childNodes{documentFragment.childNodes,document.querySelectorAll(),}
+  //htmlCollection{documentFragment.children,document.getElementByTagName(),...}
+  let removeList = [];
+  for (let index = 0; index < collection.length; index++) {
+    let el = collection[index]
+    if (el.innerText === '') {
+      removeList.push(el)
+    }
+  }
+  for (let [index,
+    element]of removeList.entries()) {
+    element.remove();
+  }
+}
+
+/**
+ * [description]
+ * @param  {[type]} tag        [description]
+ * @param  {[type]} collection [description]
+ * @return {[type]}            [description]
+ */
+const addTag = function(tag, collection) {
+  //childNodes{documentFragment.childNodes,document.querySelectorAll(),}
+  //htmlCollection{documentFragment.children,document.getElementByTagName(),...}
+  //supposed to be a span collection
+  let removeList = [];
+  for (let index = 0; index < collection.length; index++) {
+    let el = collection[index]
+    if (el.innerText == '') {
+      removeList.push(el);
+    } else {
+      el.innerHTML = `<${tag}>${el.innerHTML.replace(`<${tag}>`, '').replace(`</${tag}>`, '')}</${tag}>`
+    }
+  }
+  for (let [index,
+    element]of removeList.entries()) {
+    element.remove();
+  }
+}
+
+/**
+ * [description]
+ * @param  {[type]} tag        [description]
+ * @param  {[type]} collection [description]
+ * @return {[type]}            [description]
+ */
+const removeTag = function(tag, collection) {
+  //childNodes{documentFragment.childNodes,document.querySelectorAll(),}
+  //htmlCollection{documentFragment.children,document.getElementByTagName(),...}
+  let removeList = [];
+  for (let index = 0; index < collection.length; index++) {
+    let el = collection[index]
+    if (el.innerText === '') {
+      removeList.push(el);
+    } else {
+      console.log(el.innerHTML);
+      el.innerHTML = el.innerHTML.replace(`<${tag}>`, '').replace(`</${tag}>`, '')
+    }
+  }
+  for (let [index,
+    element]of removeList.entries()) {
+    element.remove();
+  }
+}
+
+/**
+ * [description]
+ * @param  {[type]} tag        [description]
+ * @param  {[type]} collection [description]
+ * @return {[type]}            [description]
+ */
+const addTagInPara = function(tag, collection) {
+  // let removeList = [];
+  for (let index = 0; index < collection.length; index++) {
+    let el = collection[index]
+    // if (el.innerText === '') {
+    //   if (index != 0 || index != collection.length) {
+    //     removeList.push(el)
+    //   }
+    // } else {
+    addTag(tag, el.children);
+    // }
+
+  }
+  // for (let [index,element] of removeList.entries()) {
+  //   element.remove();
+  // }
+}
+
+/**
+ * [description]
+ * @param  {[type]} tag        [description]
+ * @param  {[type]} collection [description]
+ * @return {[type]}            [description]
+ */
+const removeTagInPara = function(tag, collection) {
+  // let removeList = [];
+  for (let index = 0; index < collection.length; index++) {
+    let el = collection[index]
+    // if (el.innerText === '') {
+    //   if (index != 0 || index != collection.length) {
+    //     removeList.push(el)
+    //   }
+    // } else {
+    removeTag(tag, el.children);
+    // }
+
+  }
+  // for (let [index,element] of removeList.entries()) {
+  //   element.remove();
+  // }
+}
+
+const isSame = function(obj, foo) {
+  let result = false;
+  let oarr,
+    farr;
+  oarr = Object.keys(obj).sort();
+  farr = Object.keys(foo).sort();
+  if (oarr.toString() == farr.toString()) {
+    result = true;
+    for (let key of oarr) {
+      if (typeof obj[key] == 'object') {
+        result = isSame(obj[key], foo[key]) && result;
+      } else {
+        result = (obj[key] == foo[key]) && result;
+      }
+    }
+    return result;
+  } else {
+    return false;
+  }
+}
+
+module.exports= {
+  isAllElements,
+  matchById,
+  matchByTag,
+  matchByClass,
+  judgeStart,
+  removeEmpty,
+  addTag,
+  removeTag,
+  addTagInPara,
+  removeTagInPara,
+  isSame
+}
