530b47acc17647d4dbac3c8f394c87c889ecacfb
MBP <sun1993hlj@hotmail.com>
2017-09-07 17:19:41 +0800

    add listener for element

--- src/sqedt.js
+++ src/sqedt.js
@@ -1,6 +1,9 @@
 'use strict';
+require('babel-polyfill')
 
 var utils = require('./utils');
+var state = require('./SelectState')
+var selectEvent = require('./selectEvent')
 
 var DEFAULT_CONFIG = {};
 
@@ -22,6 +25,48 @@ const SQEdt=(el,option)=>{
   let edt = createInstance(Object.assign({
     el: el
   }, option, DEFAULT_CONFIG));
+
+  //when a para is empty, cell will be replace with this
+  el.addEventListener('keyup', e => {
+    let sl = window.getSelection();
+    if (e.keyCode === 8) {
+      if (sl.isCollapsed) {
+        let para = getParent(sl.anchorNode, '.para');
+        if (para && para.innerHTML == '<br>' && e.keyCode === 8) {
+          para.innerHTML = '<span style="font-size:16px;"><br></span>';
+          sl.getRangeAt(0).selectNode(para.firstElementChild)
+        }
+      }
+    }
+  })
+
+  //add selectionchange to inform UI
+  document.addEventListener('selectionchange', e => {
+    if (document.activeElement == el) { //judge the cursor in the el
+      let state = handleSelection(edt.sl).export();
+      let same = utils.isEqual(edt.slState, state);
+      if (!same) {
+        edt.slState = state;
+        edt.callbackInChange(state, e); //call the callback to subscriber
+      }
+    }
+  })
+
+  //cause of the function not fully prepared
+  el.addEventListener('copy', e => {
+    e.returnValue = false;
+    return false;
+  })
+
+  el.addEventListener('paste', e => {
+    e.returnValue = false;
+    return false;
+  })
+
+  el.addEventListener('cut', e => {
+    e.returnValue = false;
+    return false;
+  })
 
   return edt;
 }
