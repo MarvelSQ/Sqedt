c7a4f3c83ecb7416f17f94a1db1aaf7bf59ce729
MBP <sun1993hlj@hotmail.com>
2017-09-07 17:18:24 +0800

    new selectState

--- src/SelectState.js
+++ src/SelectState.js
@@ -1,16 +1,43 @@
-require('babel-polyfill')
 /**
  * this state is editor's select_state
  * @constructor
  */
-function State(){
-  this.b= true;
-  this.u= true;
-  this.d= true;
-  this.i= true;
-  this.size= new Set();
-  this.justify= new Set();
-  this.color= new Set();
-}
+ function SlState() {
+   this.b = true;
+   this.u = true;
+   this.d = true;
+   this.i = true;
+   this.size = new Set();
+   this.justify = new Set();
+   this.color = new Set();
+ }
 
-module.exports = State;
+ SlState.prototype.export = function() {
+   let s = {
+     allB: this.b,
+     allU: this.u,
+     allD: this.d,
+     allI: this.i,
+     allSize: this.size.size == 1,
+     allColor: this.color.size == 1,
+     allJustify: this.justify.size == 1,
+     sizes: Array.from(this.size),
+     colors: Array.from(this.color),
+     justifys: Array.from(this.justify),
+     toString() {
+       let str = '';
+       Object.keys(this).forEach(key => {
+         if (key.endsWith('s')) {
+           str += key + ':[' + this[key].toString() + '];';
+         } else {
+           str += key + ':' + this[key] + ';';
+         }
+       })
+       return str;
+     }
+   }
+   Object.defineProperty(s, 'toString', {enumerable: false})
+   return s;
+ }
+
+module.exports = SlState;
